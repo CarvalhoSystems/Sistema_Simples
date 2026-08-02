14adb3fc790b1fa9ddd1c5e8970359ebd1973229
Rodrigo de Carvalho <carvalho_borges@icloud.com>
2026-08-02 03:56:31 -0300

    mudado o vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import HHTPS from "@vitejs/plugin-basic-ssl";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
