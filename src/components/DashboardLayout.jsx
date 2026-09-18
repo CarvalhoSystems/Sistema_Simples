ac49907d1e935bc066000793fb69581e1eacec81
Rodrigo de Carvalho <carvalho_borges@icloud.com>
2026-09-18 00:56:23 -0300

    fix: atualiza case do arquivo Dashboard.css para a Vercel

--- src/components/DashboardLayout.jsx
+++ src/components/DashboardLayout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
-import "./Dashboard.css";
+
 
 export default function DashboardLayout() {
   return (
