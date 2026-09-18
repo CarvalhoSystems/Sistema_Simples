ea02bd12c474fb03228c531d61902cdfcd05696a
Rodrigo de Carvalho <carvalho_borges@icloud.com>
2026-09-18 01:06:14 -0300

    fix: atualiza case do arquivo Dashboard.css para a Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { AuthProvider } from "./components/AuthContext.jsx";
 import App from "./App.jsx";
 
 import "./index.css";
-import "./components/dashboard.css"; // Importa o novo estilo do dashboard
+import "./components/Dashboard.css";// Importa o novo estilo do dashboard
 import "./components/relatorio.css"; // Importa o estilo da página de relatórios
 import "./pages/config.css"; // Importa o estilo da página de configurações
 import "./pages/suporte.css"; // Importa o estilo da página de suporte
